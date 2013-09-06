24e2aa8ba8047d59ec496c404fb1f671cd054251
Jeferson Silva <jeferson.silva900@gmail.com>
2013-09-06 01:42:18 -0300

    teste ok

--- src/br/com/radaction/Relogio.java
+++ src/br/com/radaction/Relogio.java
@@ -1,13 +1,35 @@
 package br.com.radaction;
-public class Relogio{
+
+public class Relogio {
 
 	/**
-	 * O objetivo do é passar as horas e segundos(int) e retornar um string no formato convecional
-	 * @param hora Total de Horas em segundos 
+	 * O objetivo do é passar as horas e segundos(int) e retornar um string no
+	 * formato convecional
+	 * 
+	 * @param hora
+	 *            Total de Horas em segundos
 	 * @return hora no formato convecional 00:00:00
 	 */
-	public String mostraHora(int hora){
-		return "00:00:00";
+	public String mostraHora(int hora) {
+		int minutos = hora / 60;
+		int segundos = hora % 60;
+		int horas = minutos / 60;
+		int min = minutos % 60;
+		String strMinuto;
+		if (min < 10) {
+			strMinuto = "0" + min;
+		} else {
+			strMinuto = "" + min;
+		}
+		String strSegundos;
+		if (segundos < 10) {
+			strSegundos = "0" + segundos;
+		} else {
+			strSegundos = "" + segundos;
+		}
+
+		return horas + ":" + strMinuto + ":" + strSegundos;
+
 	}
 
 }
--- src/br/com/radaction/test/RelogioTest.java
+++ src/br/com/radaction/test/RelogioTest.java
@@ -10,13 +10,13 @@ public class RelogioTest {
 
 	@Test
 	public void testMostraRelogio() {
-		Relogio r1=new Relogio();
-		Relogio r2=new Relogio();
-		String hora1=r1.mostraHora(82800);
-		String hora2=r2.mostraHora(81120);
-		assertEquals(hora1, "23:00:00");
-		assertEquals(hora2, "22:32:00");
-	
+		Relogio r1 = new Relogio();
+		Relogio r2 = new Relogio();
+		String hora1 = r1.mostraHora(82800);
+		String hora2 = r2.mostraHora(81120);
+		assertEquals("23:00:00", hora1);
+		assertEquals("22:32:00", hora2);
+
 	}
 
 }
